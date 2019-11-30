6c17ec61e310cad3cf642239b0fee9021cd10278
Fedya <fedor.chuprakov@mail.ru>
2019-11-30 23:48:37 +0300

    Uploaded

---
+++ cmake-build-debug/CMakeFiles/UTF8.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UTF8.dir/main.c.o"
+  "UTF8.pdb"
+  "UTF8"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/UTF8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
